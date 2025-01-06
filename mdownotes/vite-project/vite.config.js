a2e55c980a41ad0769559dadf57c52126c94e942
dmatthw <dmatthw@umich.edu>
2025-01-05 19:55:13 -0500

    commit before playing with a script that deletes stuff

--- mdownotes/vite-project/vite.config.js
+++ mdownotes/vite-project/vite.config.js
@@ -5,4 +5,20 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   mode: "development",
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Customize the name of the entry JavaScript file
+        entryFileNames: 'assets/bundle.js', // Change this to your desired name
+
+        // Customize the name of the CSS file
+        assetFileNames: (assetInfo) => {
+        if (assetInfo.name.endsWith('.css')) {
+          return 'assets/style.css'; // Custom name for CSS file
+        }
+        return 'assets/[name].[hash][extname]'; // Default for other assets
+      },
+      },
+    },
+  },
 })
