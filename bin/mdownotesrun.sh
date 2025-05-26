4ba306df0c9ec42957babd0a839dc5a2a4dfb240
mdowling315 <dmatthw@umich.edu>
2025-05-26 18:54:12 -0400

    changes for usability purposes

--- bin/mdownotesrun.sh
+++ bin/mdownotesrun.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
 set -Eeuo pipefail
-
-cd ~/mdownotes
+set -x 
+cd ~/mdownotespublic
 if [ -e "var/mdownotes.sqlite3" ]; then
     set -x
     # build the vite project embbedded in this application
-    cd ~/mdownotes/mdownotes/vite-project
+    cd ~/mdownotespublic/mdownotes/vite-project
     npm run build
     rm -rf ../static/assets
     mv dist/assets ../static/assets/
     # swtich back and then run the app
-    cd ~/mdownotes
+    cd ~/mdownotespublic
     flask --app mdownotes --debug run --host 0.0.0.0 --port 8000
 else
     echo "Error: can't find database var/mdownotes.sqlite3"
